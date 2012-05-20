41b56464d8ef9dcc536da0ebffad3596ec4c03ac
Ben Hirsch <benhirsch24@gmail.com>
2012-05-20 15:11:45 -0700

    consolidate project

--- extract_allele.py
+++ extract_allele.py
@@ -6,7 +6,6 @@ import re
 from optparse import OptionParser
 import ConfigParser
 
-
 DEBUG = True
 
 '''
@@ -16,14 +15,12 @@ parameters. These alleles can be represented as DNA sequences, or alternatively
 as pyroprints (histograms). Biologically speaking, an allele suggests a genetic
 indication of a different strain (I think?).
 '''
-def extractAlleles():
-   #Parse arguments and retrieve appropriate variables
-   (dataDir, disp, primer, numIsolates) = handleArgs()
+def extractAlleles(dataDir, disp, primer, numIsolates):
 
    #Expand an abbreviated "dispensation sequence"
    #e.g. 2(ATCG) expands to ATCGATCG
    dispSeq = expandSequence(disp)
-  
+
    #Find files that contain actual DNA sequence data
    validSequenceFiles = findSequenceFiles(dataDir)
    #From DNA sequence files, extract the actual DNA sequences
@@ -38,61 +35,19 @@ def extractAlleles():
    alleleFiles = []
    allelePeaks = []
 
+   # find unique strings
    for (seqFile, seqStr, seqPeaks) in seqList:
       if seqStr not in alleles:
          alleles.append(seqStr)
          alleleFiles.append(seqFile)
          allelePeaks.append(seqPeaks)
 
-   for alleleFile, allele, peak in map(None, alleleFiles, alleles, allelePeaks):
-      print "allele '{0}' from file '{1}'\n\thas pyroprint '{2}'\n".format(allele,
-            alleleFile, peak)
+  # for alleleFile, allele, peak in map(None, alleleFiles, alleles, allelePeaks):
+  #    print "allele '{0}' from file '{1}'\n\thas pyroprint '{2}'\n".format(allele,
+  #          alleleFile, peak)
 
    return allelePeaks
 
-'''
-This method is parses command-line arguments. It returns a directory containing
-DNA sequences (or other directories) a dispensation sequence to use on given
-DNA sequences, a forward primer (which may need to be applied to the reverse
-complement strand) and an upper bound on the number of isolates to generate in
-silico.
-'''
-def handleArgs():
-   parser = OptionParser(usage="Extracts alleles from input DNA sequences\n")
-   parser.add_option("--path", dest="path", default="Genome Sequences/",
-                     help="Path containing DNA sequences")
-   parser.add_option("--disp", dest="dispSeq", default="AACACGCGA23(GATC)GAA",
-                     help="Dispensation Sequence")
-   parser.add_option("-n", dest="num", type="int", default=-1,
-                     help="Number of Isolates to generate")
-   parser.add_option("-f", dest="file", default="parameters.config",
-                     help="File containing parameters")
-   parser.add_option("--fp", dest="forPrimer", default="AACCTT",
-                     help="Forward Primer")
-
-   (options, args) = parser.parse_args()
- 
-   if options.file:
-      config = ConfigParser.RawConfigParser()
-      config.read(options.file)
-      dataPath = config.get("params", "path")
-    
-      if config.has_option("params", "num"):
-         numIsolates = config.getint("params", "num")
-      else:
-         numIsolates = -1
-
-      forwardPrimer = config.get("params", "forPrimer")
-      dispSeq = config.get("params", "dispSeq")
-
-   else:
-      #Use command line args
-      dataPath = options.path
-      numIsolates = options.num
-      dispSeq = options.dispSeq
-      forwardPrimer = options.primer
-
-   return (dataPath, dispSeq, forwardPrimer, numIsolates)
 
 
 def findSequenceFiles(dataDir):
@@ -149,7 +104,7 @@ def pyroprintSequences(allSequences, dispSeq, primer):
 
 def extractFileSequences(sequenceFiles):
    allSequences = []
-  
+
    for sequenceFile in sequenceFiles:
       with open(sequenceFile) as f:
          text = f.read()
@@ -201,7 +156,7 @@ def reverseSeq(seq):
 # seqExp should be in the format of [StartSeq](NumRepeat)[RepeatedSeq]
 def expandSequence(seqExp):
    seq = re.findall('[a-zA-Z]+|\d+\([a-zA-Z]+\)', seqExp)
-        
+
    complete = ''
    for item in seq:
       if re.match('\d',item):
