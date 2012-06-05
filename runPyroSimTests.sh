859771e5dd44ebcf514ab9f4d2e2fa51048b5bf9
Drin <DrinMontana@gmail.com>
2012-06-05 12:03:43 -0700

    Modified the script to be a little bit more involved so that it's easier to modify.
    Also important to note, that when running on 4 devices, it is still true that only 1 profiling output is written. No Idea why this is, but user (profiler?) be wary!

--- runPyroSimTests.sh
+++ runPyroSimTests.sh
@@ -1,4 +1,25 @@
-python parseDNAFile.py --memory=globalMem --device=0 -m 10 > 0_global_10.out &
-python parseDNAFile.py --memory=globalMem --device=1 -m 10 > 1_global_10.out &
-python parseDNAFile.py --memory=constantMem --device=2 -m 10 > 0_constant_10.out &
-python parseDNAFile.py --memory=constantMem --device=3 -m 10 > 1_constant_10.out &
+#!/bin/bash
+export CUDA_PROFILE=1
+export CUDA_PROFILE_CSV=1
+export CUDA_PROFILE_CONFIG=cuda_profile.cfg
+
+PROFILING_SETTINGS="CUDA_PROFILE=1 CUDA_PROFILE_CSV=1 CUDA_PROFILE_CONFIG=cuda_profile.cfg"
+
+TEST1_PARAMS="--memory=globalMem --device=0 -m -1"
+TEST2_PARAMS="--memory=globalMem --device=1 -m -1"
+TEST3_PARAMS="--memory=constantMem --device=2 -m -1"
+TEST4_PARAMS="--memory=constantMem --device=3 -m -1"
+
+TEST1_OUT="0_global_full.out"
+TEST2_OUT="1_global_full.out"
+TEST3_OUT="0_constant_full.out"
+TEST4_OUT="1_constant_full.out"
+
+if [ ! -f $TEST1_OUT ] && [ ! -f $TEST2_OUT ] && [ ! -f $TEST3_OUT ] && [ ! -f $TEST4_OUT ]; then
+   python parseDNAFile.py $TEST1_PARAMS > $TEST1_OUT &
+   python parseDNAFile.py $TEST2_PARAMS > $TEST2_OUT &
+   python parseDNAFile.py $TEST3_PARAMS > $TEST3_OUT &
+   python parseDNAFile.py $TEST4_PARAMS > $TEST4_OUT &
+else
+   echo "Error: Not running, don't want to clobber old results..."
+fi
